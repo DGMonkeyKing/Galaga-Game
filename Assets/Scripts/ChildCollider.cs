c73e450c6ddaa84f3feae6eb3c4c2db358ae2f0b
DGMonkeyKing <davidnew33@gmail.com>
2016-05-18 23:11:50 +0200

    Added the new galaga type name
    The new game has been added ready to be play. Mozilla Firefox browser is
    necessary to run the game because for chrome is not already supported.

---
+++ Assets/Scripts/ChildCollider.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChildCollider : MonoBehaviour {
+	void OnCollisionEnter2D( Collision2D collision) {
+		Transform parent = this.gameObject.transform.parent;
+		parent.GetComponent<Enemy_4>().SendMessage("OnCollisionEnter2D",collision);
+	}
+}
---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour {
+
+	public static GUIManager S;
+	public UnityEngine.UI.Text scoringText;
+
+	void Awake () {
+		S = this;
+	}
+	
+	// Update is called once per frame
+	public void UpdateScore (int newScore) {
+		string score = "Score: " + newScore;
+		scoringText.text = score;
+	}
+
+    public void startPressed()
+    {
+        SceneManager.LoadScene("Scene_0");
+    }
+}
